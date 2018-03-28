b1403f208e1b3ef6830a77f99311cd2ee51a0a9e
Usuário Convidado <Guest@pa06micro04.local>
2018-03-28 19:37:19 -0300

    Initial Commit

---
+++ MoviesLib/Movie.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  MoviesLib
+//
+//  Created by Usuário Convidado on 28/03/18.
+//  Copyright © 2018 EricBrito. All rights reserved.
+//
+
+import Foundation
+struct Movie {
+    let title: String
+    let categories: [String]
+    let duration: String
+    let rating: Double
+    let summary: String
+    let image_name: String
+}
---
+++ MoviesLib/MovieViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MoviesLib
+//
+//  Created by Eric on 06/03/17.
+//  Copyright © 2017 EricBrito. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    // MARK: IBOutlets
+    @IBOutlet weak var ivPoster: UIImageView!
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbGenre: UILabel!
+    @IBOutlet weak var lbDuration: UILabel!
+    @IBOutlet weak var lbScore: UILabel!
+    @IBOutlet weak var tvSinopsis: UITextView!
+    @IBOutlet weak var lcButtonX: NSLayoutConstraint!
+    
+    
+    // MARK: Super Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
